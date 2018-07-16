af0e99405126c0255b19e1f1cdac3705a538482b
Minhajul0786 <calcium4@gmail.com>
2018-07-16 18:04:24 +0530

    added ajax

--- microBlogger/blogger/urls.py
+++ microBlogger/blogger/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from blogger.views import registeration,loginUser,display_posts,create_post
+from blogger.views import registeration,loginUser,display_posts,create_post,logout, validate_username, comment
 app_name = "blogger"
 urlpatterns = [
     path('register/', registeration , name='register'),
     path('login/', loginUser , name='login'),
     path('display/', display_posts, name='display'),
     path('create/', create_post, name='create'),
+    path('ajax/validate_username/',validate_username, name = 'validate_username' ),
+    path('ajax/comment/', comment , name= 'comment'),
 ]
